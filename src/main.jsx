1716494f0605020ee0c8287223cf2a4f72461316
MayankVir <virmanimayank15@gmail.com>
2024-02-23 20:06:06 +0530

    all threee screens created using dummy data but real google auth and all state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GoogleOAuthProvider clientId="804436694887-5k63bbovlbq3vg1lrun2cje82oh0tk7k.apps.googleusercontent.com">
+      <App />
+      <Toaster />
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
